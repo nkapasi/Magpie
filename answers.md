96bda9b7d5f4308c0a8d007c44da12ce13299c68
Naman Kapasi <kapasinh@sdcsdk12.org>
2018-11-06 12:29:34 -0700

    Actiivy 2 Complete

---
+++ answers.md
@@ -0,0 +1,8 @@
+What Happens when more than one keyword pops up?
+    The one that comes first in the if block will be the one used, so higher priority goes up top
+
+1. It responds to the key word even if it is embedded inside a word. This can cause responses that make no sense.
+
+keyword - fuck - results in stop your profanity
+keyword - smash - results in no thanks your not good enough
+keyword - know - results in you dont know nothing
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -38,6 +38,19 @@ public class Magpie2 {
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if(statement.indexOf("cat") >=0
+				|| statement.indexOf("dog")>=0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Mr.Padgen") >= 0){
+			response = "He sounds like a good teacher";
+		} else if (statement.trim().length() == 0){
+			response = "Say Something Please";
+		} else if(statement.indexOf("smash")){
+			response = "No Thanks Your Not good enough";
+		} else if(statement.indexOf("Fuck")){
+			response = "Stop Your Profanity";
+		} else if(statement.indexOf("know")){
+			response = "You don't know nothing"
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +63,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,6 +76,10 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if( whichResponse == 4) {
+			response = "Okay .......";
+		} else if( whichResponse == 5){
+			response = "Really? Thats crazy.";
 		}
 
 		return response;
